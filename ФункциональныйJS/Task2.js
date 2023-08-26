d53af73934083413fac282398b51b57ca009bd26
Denis Yershov <frontsteron@gmail.com>
2023-08-26 10:48:04 +0500

    HW6 done

---
+++ ФункциональныйJS/Task2.js
@@ -0,0 +1,21 @@
+﻿function createCounter() {
+   let count = 0;
+ 
+   return {
+     increment: () => {
+       count++;
+     },
+     decrement: () => {
+       count--;
+     },
+     getCount: () => {
+       return count;
+     }
+   };
+ }
+ 
+ const counter = createCounter();
+ counter.increment();
+ counter.increment();
+ counter.decrement();
+ console.log(counter.getCount()); // Вывод: 1 
---
+++ ФункциональныйJS/Task3.js
@@ -0,0 +1,19 @@
+﻿function findElementByClass(element, targetClass) {
+   if (element.classList.contains(targetClass)) {
+     return element;
+   }
+ 
+   for (let child of element.children) {
+     const foundElement = findElementByClass(child, targetClass);
+     if (foundElement) {
+       return foundElement;
+     }
+   }
+ 
+   return null;
+ }
+ 
+ const rootElement = document.getElementById('root');
+ const targetElement = findElementByClass(rootElement, 'my-class');
+ console.log(targetElement);
+ 
