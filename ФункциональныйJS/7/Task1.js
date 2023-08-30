14ad9c5208b9ca874ffc23130dd7ae9b104755d3
Denis Yershov <frontsteron@gmail.com>
2023-08-30 15:53:12 +0500

    HW7 done

---
+++ ФункциональныйJS/7/Task1.js
@@ -0,0 +1,19 @@
+﻿class Book {
+   constructor(title, author, pages) {
+     this.title = title;
+     this.author = author;
+     this.pages = pages;
+   }
+ 
+   displayInfo() {
+     console.log(`Title: ${this.title}, Author: ${this.author}, Pages: ${this.pages}`);
+   }
+ }
+ 
+ // Создание экземпляра класса Book
+ const book1 = new Book("The Great Gatsby", "F. Scott Fitzgerald", 180);
+ const book2 = new Book("To Kill a Mockingbird", "Harper Lee", 320);
+ 
+ // Использование метода displayInfo()
+ book1.displayInfo();
+ book2.displayInfo(); 
