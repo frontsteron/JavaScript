9a63e28e9532c586a51ec1786cb2755ce00411b5
Denis Yershov <frontsteron@gmail.com>
2023-09-03 10:22:29 +0500

    HW8 done

---
+++ ФункциональныйJS/8/Task1.js
@@ -0,0 +1,27 @@
+﻿class Employee {
+   constructor(name) {
+     this.name = name;
+   }
+ 
+   displayInfo() {
+     console.log(`Name: ${this.name}`);
+   }
+ }
+ 
+ class Manager extends Employee {
+   constructor(name, department) {
+     super(name);
+     this.department = department;
+   }
+ 
+   displayInfo() {
+     console.log(`Name: ${this.name}\nDepartment: ${this.department}`);
+   }
+ }
+ 
+ // Пример использования классов
+ const employee = new Employee("John Smith");
+ employee.displayInfo();
+ 
+ const manager = new Manager("Jane Doe", "Sales");
+ manager.displayInfo(); 
