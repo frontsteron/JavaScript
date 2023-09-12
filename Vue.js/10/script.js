e89c51e3cb31d561682d9d034f90244b5823b11c
Denis Yershov <frontsteron@gmail.com>
2023-09-12 09:57:05 +0500

    HW10 done

---
+++ Vue.js/10/script.js
@@ -0,0 +1,28 @@
+﻿// Инициализируем новый экземпляр Vue
+const app = Vue.createApp({
+   data() {
+     return {
+       buttonText: "Перевернуть",
+       isReversed: false,
+       list: [],
+       newItem: "Новый элемент списка",
+     };
+   },
+   methods: {
+     // Метод для переворачивания текста кнопки
+     reverseButtonText() {
+       this.buttonText = this.buttonText.split("").reverse().join("");
+     },
+     // Метод для добавления нового элемента в список
+     addItemToList() {
+       this.list.push(this.newItem);
+     },
+     // Метод для удаления элемента из списка
+     removeItem(index) {
+       this.list.splice(index, 1);
+     },
+   },
+ });
+ 
+ // Монтируем приложение на странице
+ app.mount("#app"); 
