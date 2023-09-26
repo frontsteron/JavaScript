383fd1a643540f682c942e28b49be08f79b0fd32
Denis Yershov <frontsteron@gmail.com>
2023-09-26 12:13:44 +0500

    HW14 done

---
+++ Vue.js/productdetails/src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="app">
+    <ProductDetails :product="productData" />
+  </div>
+</template>
+
+<script>
+import ProductDetails from './components/ProductDetails.vue';
+
+export default {
+  data() {
+    return {
+      productData: {
+        name: 'Example Product',
+        price: 99.99,
+        available: true,
+      },
+    };
+  },
+  components: {
+    ProductDetails,
+  },
+};
+</script>
---
+++ Vue.js/productdetails/src/components/ProductDetails.vue
@@ -0,0 +1,32 @@
+﻿<template>
+   <div>
+     <h2>{{ product.name }}</h2>
+     <p>Price: {{ formattedPrice }}</p>
+     <p>Status: {{ product.available ? 'Available' : 'Out of stock' }}</p>
+   </div>
+ </template>
+ 
+ <script>
+ export default {
+   props: {
+     product: {
+       type: Object,
+       required: true,
+     },
+   },
+   computed: {
+     formattedPrice() {
+       // Функция для форматирования числа в строку с знаком валюты
+       const formatter = new Intl.NumberFormat('en-US', {
+         style: 'currency',
+         currency: 'USD', // Здесь можно указать нужную валюту
+         minimumFractionDigits: 2, // Количество знаков после запятой
+       });
+ 
+       // Форматируем цену продукта и возвращаем результат
+       return formatter.format(this.product.price);
+     },
+   },
+ };
+ </script>
+ 
